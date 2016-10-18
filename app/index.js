0cdfd9dd3a384c44732785bff45e1a722a947c8a
Arun <arunkhanna00@gmail.com>
2016-10-18 16:29:49 -0700

    add comments

--- app/index.js
+++ app/index.js
@@ -1,5 +1,7 @@
+// Create Angular Module
 var myApp = angular.module("myApp", []);
 
+// Create Angular Controller
 myApp.controller("mainController", ["$scope", function($scope) {
 	$scope.message = "Hello World";
 }]); 
